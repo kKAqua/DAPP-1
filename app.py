e70a53a27c79c0c82ae6c1b5357f14c6e78dd424
Haifeng Zhang <aqua.z@foxmail.com>
2024-09-12 15:05:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/secondary',methods=["get","post"])
+def index():
+    return render_template('second.html')
+
 if __name__=='__main__':
     app.run()
